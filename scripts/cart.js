dcff78083e3d929bcdfbb5e80c6758ee197ec241
prince pundir <princepundir125@gmail.com>
2025-12-30 14:49:37 +0530

    add checkout  using js

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,1 +1,8 @@
-export const cart =[];
+export const cart =[{
+   productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+   number: 2
+},{
+    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    number : 1
+}];
+
