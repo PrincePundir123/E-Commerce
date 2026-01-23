c49af9c5246f0556bc08219037c7807e135a56ef
prince pundir <princepundir125@gmail.com>
2026-01-23 11:25:14 +0530

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -8,6 +8,7 @@ import { loadCart } from "../data/cart.js";
 
 // use Promiseall([])
 
+/*
 Promise.all([
    loadProductsFetch(),
     new Promise((resolve) => {
@@ -22,19 +23,35 @@ Promise.all([
     renderOrder();
     renderPaymentSummary();
 })
+*/
+
+
+async function loadPages(){
+    await loadProductsFetch();
+    const value = await new Promise((resolve) => {
+       // bhai is stored in value 
+        loadCart(() => {
+            resolve("bhai");  
+        });
+    })
+    renderOrder();
+    renderPaymentSummary();
+  }
+   
+loadPages();
 
 
 
 // Promise.all([
-//     new Promise((resolve) => {
-//         console.log("1");
-//         loadProducts(() => {
-//             console.log("3");
-//             resolve("hello");
-//         });
-//         console.log("2");
-//     }),
-//     new Promise((resolve) => {
+    //     new Promise((resolve) => {
+        //         console.log("1");
+        //         loadProducts(() => {
+            //             console.log("3");
+            //             resolve("hello");
+            //         });
+            //         console.log("2");
+            //     }),
+            //     new Promise((resolve) => {
 //         console.log("4");
 //         loadCart(() => {
 //             resolve("bhai");
@@ -42,41 +59,79 @@ Promise.all([
 //         });
 //     })
 // ]).then((value) => {
-//     console.log(value);
+    //     console.log(value);
 //     renderOrder();
 //     renderPaymentSummary();
 // })
 
 
 // new Promise((resolve) =>{
-//     console.log("1");
+    //     console.log("1");
 //     loadProducts(() =>{
 //         console.log("3");
 //         resolve();
 //     });
 //     console.log("2");
 // }).then(()=>{
-//     return new Promise((resolve) =>{
-//     console.log("4");
-//         loadCart(()=>{
-//             resolve();
-//     console.log("5");
-
-//         });
+    //     return new Promise((resolve) =>{
+        //     console.log("4");
+        //         loadCart(()=>{
+            //             resolve();
+            //     console.log("5");
+            
+            //         });
 //     });
 // }).then(()=>{
-//     renderOrder();
-//     renderPaymentSummary();
-// })
-
-
-
-// loadProducts(()=>{
+    //     renderOrder();
+    //     renderPaymentSummary();
+    // })
+    
+    
+    
+    // loadProducts(()=>{
 //     loadCart(() =>{
 //         renderOrder();
 //         renderPaymentSummary();
 //     });
 // });
-
-
 // loadProductsFetch();
+
+
+
+/*
+function loadPages(){
+    new Promise ((resolve) =>{
+        console.log("hello");
+    resolve();
+  }).then(() =>{
+      console.log("Bhai");
+      console.log("Bhai");
+      console.log("Bhai");
+  })
+}
+loadPages();
+*/
+/*
+function loadPages(){
+   return  new Promise ((resolve) =>{
+        console.log("hello");
+    resolve();
+  })
+}
+loadPages().then(() =>{
+      console.log("Bhai");
+      console.log("Bhai");
+      console.log("Bhai");
+  });
+  */
+
+  /*
+  async function loadPages(){
+     console.log("hello");
+  }
+  loadPages().then(() =>{
+      console.log("Bhai");
+      console.log("Bhai");
+      console.log("Bhai");
+  });
+  */
